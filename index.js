560fbe003bbb6c15a7dbce69d2cd11f2eb31f068
magalhaes3 <joo.magalhes15@gmail.com>
2022-10-12 21:52:31 +0100

    Make results page show brand, model, and car price

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-function request() {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-
-    const car = urlParams.get('car');
+function request(car) {
     const urlmaster = "https://europe-west1-fire-functions-b6ad7.cloudfunctions.net/autoScout24?url="
     const requrl = urlmaster + car;
 
@@ -10,5 +6,33 @@ function request() {
     xmlHttp.open("GET", requrl, false);
     xmlHttp.send();
 
+    showResult();
     return JSON.parse(xmlHttp.responseText);
+}
+
+function showResult() {
+    var x = document.getElementById("results-section");
+    if (x.style.display === "none") {
+        x.style.display = "grid";
+    } else {
+        x.style.display = "none";
+    }
+}
+function fillCarDetails(car) {
+    const carName = document.getElementById("car-name");
+    carName.textContent = car.brand + " " + car.model;
+    const price = document.getElementById("price-car-text");
+    price.textContent = car.price;
+}
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+const car = urlParams.get('car');
+if (car === "" || car === null) {
+    console.log("Car not found...");
+} else {
+    let carResult = request(car);
+    console.log(carResult);
+    fillCarDetails(carResult);
 }
