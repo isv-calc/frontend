f49a216eca504eb1ef31b3f7086431c915c147c3
magalhaes3 <joo.magalhes15@gmail.com>
2022-10-16 13:35:36 +0100

    Add one test

---
+++ tests/test.isv.js
@@ -0,0 +1,25 @@
+import { calculateISV } from "../index.js";
+var expect = chai.expect;
+// var assert = chai.assert;
+describe('Test ISV Calculations', () => {
+    it('#1 Test', () => {
+        let isv = 100;
+        let car = {
+            brand: 'Ford',
+            displacement: 998,
+            emissions: 94,
+            fuel: 'Electric/Gasoline',
+            isWltp: undefined,
+            mileage: 5000,
+            model: 'Fiesta',
+            month: 3,
+            particulateFilter: false,
+            price: 18690,
+            year: 2022
+        };
+        let myIsv = calculateISV(car);
+        expect(myIsv).to.equal(isv);
+        // assert.deepEqual(isv, myIsv);
+
+    })
+});
